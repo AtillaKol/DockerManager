52bc685091f3339c11af830a3668fbf599862923
AtillaKol <atillakolali22@gmail.com>
2019-09-09 16:56:07 +0200

    Inital commit of Backend

---
+++ Backend/src/main/java/ch/kolali/atilla/dockermanager/backend/RESTController/RESTContollerDocker.java
@@ -0,0 +1,31 @@
+/* -----------------------------------------------------------------------------
+* RESTContollerDocker.java
+*
+* This class will define all REST-End points for Docker.
+*
+*
+* Author: Atilla Kolali
+* Version: 1.0
+*
+* History:
+* Version       Date           Who                Changes
+* 1.0           09.09.2019     Atilla Kolali      Created RESTContollerDocker
+*
+*
+* Copyright © 2019 Atilla Kolali. All rights reserved
+*
+----------------------------------------------------------------------------- */
+package ch.kolali.atilla.dockermanager.backend.RESTController;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * This class will define all REST-End points for Docker.
+ *
+ * @author Atilla Kolali
+ * @version 1.0
+ */
+@RestController
+public class RESTContollerDocker {
+
+}
