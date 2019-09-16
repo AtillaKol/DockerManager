a7e22a38737ab4cabca19e7cc47e3fc5e0bbdefe
Atilla Kolali <43301926+AtillaKol@users.noreply.github.com>
2019-09-16 22:49:18 +0200

    Delete test.py

--- ConfigurationService/iniHandler/test.py
+++
@@ -1,9 +0,0 @@
-import os
-import configparser
-
-
-path = os.path.dirname(os.path.abspath(__file__))
-config = configparser.ConfigParser()
-config.read("{}{}".format(path, "/../ini-Files/host.ini"))
-penis = config['Host']['hostname']
-print(penis)
