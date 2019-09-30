5058abe65f7b01ba084131e355f0120cd734bb80
AtillaKol <atillakolali22@gmail.com>
2019-09-30 15:28:22 +0200

    Added response which will show the response from the configservice

---
+++ frontend/src/Components/Configuration/Response/Response.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+import './Response.css';
+
+class Response extends Component {
+	render(){
+		return(
+			<div className="test">
+				Hello from Response
+			</div>
+		)
+	}
+}
+
+export default Response;
